6b0db22083371af577801f9ae8835d0634ce0213
Noah Hilverling <noah.hilverling@icinga.com>
2019-11-04 15:09:57 +0100

    Fix project path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+set -e
+
+if [ -z "${IMPORT}" ]; then
+  IMPORT="${GITHUB_REPOSITORY}"
+fi
+WORKDIR="${GOPATH}/src/github.com/${GITHUB_REPOSITORY}"
+
+mkdir -p "$(dirname "${WORKDIR}")"
+ln -s "${PWD}" "${WORKDIR}"
+cd "${WORKDIR}"
+
 dep ensure
 
 go fmt $(go list ./... | grep -v /vendor/)
